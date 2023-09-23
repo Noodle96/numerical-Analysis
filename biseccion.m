d2482c5ee993add52c6ad0b2372019b35ac983ef
Noodle96 <jorgealfredomarino2@gmail.com>
2023-09-23 10:08:39 -0500

    add biseccion implementation

---
+++ biseccion.m
@@ -0,0 +1,17 @@
+function [root, iteraciones] = biseccion(left,right,tolerancia)
+    iteraciones = 1;
+    while(right-left >= tolerancia)
+        iteraciones++;
+        middle = left + (right-left)/2;
+        if(f(middle)*f(right) > 0)
+            right = middle;
+        else
+            left = middle;
+        endif
+    endwhile
+    root = left + (right-left)/2;
+endfunction
+
+function ans = f(x)
+    ans = sqrt(x) -5*exp(-x);
+endfunction
