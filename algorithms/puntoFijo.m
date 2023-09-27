643368e52a3666624370f3f44b1d8e7bd1180fc2
Noodle96 <jorgealfredomarino2@gmail.com>
2023-09-26 22:37:19 -0500

    add puntoFijo algorithmo

---
+++ algorithms/puntoFijo.m
@@ -0,0 +1,17 @@
+function [root, iteraciones] = puntoFijo(x, tolerancia)
+    iteraciones = 0;
+    while( abs(x-theta(x)) >= tolerancia)
+        iteraciones = iteraciones +1;
+        x = theta(x);
+    end
+    root = x;
+end
+
+function x = theta(x)
+    $x = 1/(2+log(x))+1;
+    %x = (3+log(x))/(2+log(x));
+   
+   x = e^((3-2*x)/(x-1));
+   %x = e^(1/(x-1)) /e^2; 
+   %x = e^(1+(2-x)/(x-1)) / e^2;
+end
