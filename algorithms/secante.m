3a3718db0f1c7c4c1b7630bae4fe571692cc1b95
Noodle96 <jorgealfredomarino2@gmail.com>
2023-09-26 23:27:12 -0500

    add secante algorithm

---
+++ algorithms/secante.m
@@ -0,0 +1,18 @@
+function [root, iteraciones] = secante(x1, x0, tolerancia)
+    iteraciones = 0;
+    while( abs(x1 - theta(x1,x0)) >= tolerancia)
+        iteraciones = iteraciones +1;
+        x2 = theta(x1,x0);
+        x0 = x1;
+        x1 = x2;
+    end
+    root = x1;
+end
+
+function answer = theta(x1,x0)
+    answer = x1 - (f(x1)*(x1-x0))/(f(x1)-f(x0));
+end
+
+function answer = f(x)
+    answer = x - (1/(2+log(x)))-1;
+end
