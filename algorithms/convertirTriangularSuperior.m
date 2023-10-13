2240ffd3b726fc3b517e1b53ed6ceb38592c0019
Noodle96 <jorgealfredomarino2@gmail.com>
2023-10-13 18:46:00 -0500

    Algoritmo para triangulizar un sistema de ecuaciones

---
+++ algorithms/convertirTriangularSuperior.m
@@ -0,0 +1,18 @@
+%{
+    Tag: Triangulizar el sistema
+    Este proceso es obligatorio para el metodo
+    de Gauss.
+%}
+function [A,b] = convertirTriangularSuperior(A,b)
+    n = length(A);
+    for k=1:n-1 % diagonals
+        for i=k+1:n % work in row i
+            m = A(i,k)/A(k,k);
+            A(i,k) = 0;
+            for j=k+1:n %update all row i
+                A(i,j) = A(i,j)-m*A(k,j);
+            end
+            b(i) = b(i) -m*b(k);
+        end
+    end
+end
