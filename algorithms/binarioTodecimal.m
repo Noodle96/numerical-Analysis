1bc8225c5b306b24a512f8b6103ba2af0e716524
Noodle96 <jorgealfredomarino2@gmail.com>
2023-10-14 18:55:42 -0500

    convertir un numero binario a decimal

---
+++ algorithms/binarioTodecimal.m
@@ -0,0 +1,16 @@
+function ans = binarioTodecimal(binarioNumber)
+  #disp(length(binarioNumber));
+  e = 0;
+  a = 0;
+  for i=length(binarioNumber):-1:1
+    #disp(e);
+    #disp(i)
+    #disp((binarioNumber(i)-'0'))
+    a +=  ((binarioNumber(i)-'0')* power(2,e));
+    e++;
+  endfor
+  ans = a;
+endfunction
+
+# 110
+# 0*2^0   +   1*2^1  +   1*2^2
