0dbaed699baf471f9fb2edf249735f11180b0df0
Noodle96 <jorgealfredomarino2@gmail.com>
2023-10-13 19:07:58 -0500

    Algoritmo para resolver un sistema Dado, la matriz A debe de ser triangular

---
+++ algorithms/resolverSistema.m
@@ -0,0 +1,16 @@
+%{
+    tag: Resolver el sistema dado.
+    Requisito: La matriz A tiene ser triangular
+%}
+function x = resolverSistema(A,b)
+    n = length(A);
+    x = zeros(n,1);
+    x(n) = b(n)/A(n,n);
+    for k=n-1:-1:1 % row k
+        s = 0;
+        for j=k+1:n
+            s = s + A(k,j)*x(j);
+        end
+        x(k) = (b(k)-s)/A(k,k);
+    end
+end
