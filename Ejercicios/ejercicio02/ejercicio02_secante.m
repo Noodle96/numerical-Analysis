5e3de380ec998f413b4652ea4b0c9769c51f8a60
Noodle96 <jorgealfredomarino2@gmail.com>
2024-01-18 12:11:53 -0500

    Refactoring folder

---
+++ Ejercicios/ejercicio02/ejercicio02_secante.m
@@ -0,0 +1,18 @@
+function [root, iteraciones] = ejercicio02_secante(x1, x0, tolerancia)
+    iteraciones = 0;
+    while( abs(x1 - theta(x1,x0)) >= tolerancia)
+        iteraciones = iteraciones +1;
+        x2 = theta(x1,x0);
+        x0 = x1;
+        x1 = x2;
+    end
+    root = x1;
+end
+
+function answer = theta(x1,x0)
+    answer = x1 - (f(x1)*(x1-x0))/(f(x1)-f(x0));
+end
+
+function answer = f(x)
+    answer = e^x -4*x^2;
+end
